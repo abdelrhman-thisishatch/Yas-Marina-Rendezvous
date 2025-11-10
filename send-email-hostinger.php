ec8f0c8460307d3ad3397c595bf21abb09e53502
Abdelrhman Atwa <42987724+abdelrhman39@users.noreply.github.com>
2025-11-10 16:42:37 +0200

    update smtp to gostinger

--- send-email-hostinger.php
+++ send-email-hostinger.php
@@ -181,7 +181,7 @@ function sendEmailHostinger($to_email, $subject, $message, $name, $from_email) {
         // Log success to console (for debugging)
         error_log("Email sent successfully - " . date('Y-m-d H:i:s'));
         
-        return json_encode(['data'=>['alert' => 'alert-success', 'message' => SUCCESS_MESSAGE]]);
+        return json_encode(['alert' => 'alert-success', 'message' => SUCCESS_MESSAGE]);
 
         
     } catch (Exception $e) {
